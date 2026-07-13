cf4326c759ef50c74df1d56043c7e71824f919f7
Laxmi kumari <laxmigupta40386@gmail.com>
2026-07-13 22:08:34 +0545

    kjhgfd

--- Admin/booking_monitoring.php
+++ Admin/booking_monitoring.php
@@ -133,55 +133,13 @@ WHERE status='ACTIVE'
 </head>
 
 <body>
-  <script>
-
-function viewBooking(id)
-{
-    window.location='booking_details.php?id='+id;
-}
-
-function cancelBooking(id)
-{
-    if(confirm("Cancel this booking?"))
-    {
-        window.location='cancel_booking.php?id='+id;
-    }
-}
-
-function validateFilters()
-{
-    let movie = document.querySelector('select[name="movie"]').value;
-    let date  = document.querySelector('input[name="date"]').value;
-
-    if(movie === '')
-    {
-        alert("Please select a movie.");
-        return false;
-    }
-
-    if(date === '')
-    {
-        alert("Please select a date.");
-        return false;
-    }
-
-    return true;
-}
-
-setInterval(function(){
-
-    console.log("Refreshing booking data...");
-
-},30000);
-
-</script>
 
 <div class="container">
 
 <h1 class="heading">
 🎟 Booking Monitoring
 </h1>
-<form method="GET" onsubmit="return validateFilters()">
+<form method="GET">
 
 <div class="filters">
 
@@ -192,22 +150,26 @@ placeholder="Search booking..."
 value="<?php echo $search; ?>"
 >
 
-<select name="movie">
-
-<option value="">
-All Movies
-</option>
-
-<?php while($m=mysqli_fetch_assoc($movies)){ ?>
-
-<option
-value="<?php echo $m['movie_id']; ?>"
-<?php if($movie==$m['movie_id']) echo "selected"; ?>
+<select
+    name="movie"
+    required
+    oninvalid="this.setCustomValidity('Please Select Movie')"
+    onchange="this.setCustomValidity('')"
 >
-<?php echo $m['title']; ?>
-</option>
+    <option value="">
+       All movies
+    </option>
 
-<?php } ?>
+    <?php while($m=mysqli_fetch_assoc($movies)){ ?>
+
+    <option
+        value="<?php echo $m['movie_id']; ?>"
+        <?php if($movie==$m['movie_id']) echo "selected"; ?>
+    >
+        <?php echo $m['title']; ?>
+    </option>
+
+    <?php } ?>
 
 </select>
 
@@ -217,9 +179,13 @@ name="date"
 value="<?php echo $date; ?>"
 >
 
-<button class="search-btn">
-Search
+<button type="submit" class="search-btn">
+    Search
 </button>
+
+<a href="booking_monitoring.php" class="reset-btn">
+    Reset
+</a>
 
 </div>
 
